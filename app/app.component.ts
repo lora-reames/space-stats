7c319d280bfd817d557407aa171e2a2fc8410a72
patrickr <patrick.reames@gmail.com>
2016-10-17 00:48:05 -0500

    load some fake astronauts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <ul class="astros">
+      <li *ngFor="let astro of astros">
+      {{astro.name}} onboard {{astro.craft}}
+      </li>
+    </ul>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+title = 'people in space';
+astros = ASTROS;
+}
+
+export class Astro {
+  craft: string;
+  name: string;
+
+}
+
+const ASTROS: Astro[] = [
+  { craft: 'freedom 7', name: 'Mr. Nice' },
+  { craft: 'mercury 3', name: 'Narco' },
+  { craft: 'giant penis', name: 'Bombasto' }
+];
